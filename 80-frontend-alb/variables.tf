255d5f2fe1b87a0a7646f3bc7b2f32332baa8d97
AnushA120407 <anushabalu1204@gmail.com>
2026-07-08 22:13:19 -0500

    oil and karam added

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,21 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+
+variable "app_version" {
+    default = "v3"
+}
+
+
+variable "zone_id" {
+    default = "Z01082812EWZWIPIX85QQ"
+}  
+
+variable "domain_name" {
+    default = "daws-90.shop"
+}
