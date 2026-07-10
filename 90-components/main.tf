6cc299ac483df759440265c74625024e1d8ff538
AnushA120407 <anushabalu1204@gmail.com>
2026-07-09 21:19:10 -0500

    oil and karam added

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,7 +1,6 @@
 module "components" {
     for_each = var.components
     source = "git::https://github.com/AnushA120407/terraform-roboshop-component.git?ref=main"
-
     environment = var.environment
     component = each.key
     app_version = each.value.app_version
